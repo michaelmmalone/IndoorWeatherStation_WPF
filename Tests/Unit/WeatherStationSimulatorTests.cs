52b0a1e95730dbb7ea17c414c14e1c93be006515
Mike Malone <hispeedsurfer@hotmail.com>
2016-08-24 09:14:58 -0700

    Removed comment.  Could not split "unit" and "component" tests.

--- Tests/Unit/WeatherStationSimulatorTests.cs
+++ Tests/Unit/WeatherStationSimulatorTests.cs
@@ -8,7 +8,6 @@ namespace Tests.Unit
     [TestClass]
     public class WeatherStationSimulatorTests : WeatherStationTestsBase
     {
-        //TODO: try putting in base!
         [TestMethod]
         [TestCategory("Unit")]
         public override void MinuteChanged_RaisedWhenStartIsCalled()
